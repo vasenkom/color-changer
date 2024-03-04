74ddf60e1fb0e08b68688e7000e1d26902ddad7b
vasenkom <m.vasenko@yandex.ru>
2024-03-04 19:39:24 +0100

    create 5hex.js

--- 5.js
+++ 5.js
@@ -14,6 +14,3 @@ btn.addEventListener('click', function() {
 function get_number() {
     return Math.floor(Math.random() * colors.length);
 }
-
-//Hex color changer
-const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"]
